d32d7de8dd4d481ed237a25bece562ee128bb838
krubankssk3 <reborn4536@gmail.com>
2026-06-16 14:39:45 +0700

    Update API_URL in config.js with deployment URL

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
  * ตัวอย่าง: https://script.google.com/macros/s/AKfycb..../exec
  */
 window.EDU_CONFIG = {
-  API_URL: 'วาง_URL_/exec_ของคุณที่นี่'
+  API_URL: 'https://script.google.com/macros/s/AKfycbwZn6a4oyvH6lJy6fEnH-1nB_sOtwc1lzM2x_nfYsX9qEO_Kdpzxq3SIvv2B9VHKKdb/exec'
 };
